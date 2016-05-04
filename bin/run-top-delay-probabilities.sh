49b0a5e806f492e2525aef5de8a856454a1c9758
Ramunas Urbonas <r.urbonas@adform.com>
2016-05-04 18:22:09 +0300

    bins

---
+++ bin/run-top-delay-probabilities.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+set -x
+set -u
+
+spark-submit \
+    --class flight.kata.RunTopDelayProbabilities \
+    --master yarn-cluster \
+    --name TopAirports \
+    --num-executors 4 \
+    --driver-memory 512M  \
+    --driver-java-options "-XX:+UseG1GC" \
+    --executor-memory 512M \
+    --executor-cores 2 \
+    --conf 'spark.executor.extraJavaOptions=-XX:+UseG1GC' \
+    flight-kata-1.0-SNAPSHOT.jar ${1}
+
